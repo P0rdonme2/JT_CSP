4ddc4441af75cfb66ff95ef97fec679a2f80abc5
P0rdonme2 <jaxon.thompson@ucas-edu.net>
2026-09-10 14:49:44 -0600

    variable practice

--- variables_practice.py
+++ variables_practice.py
@@ -1,2 +1,13 @@
 # JT, Variables Practice
-print("Jaxon 9 121 nothing UCAS 2026 hazel 14 math")
+
+name="Jaxon"
+small_number=9
+big_number=121
+breackfast="nothing"
+fav_color="red"
+school_name="UCAS"
+year=2026
+eye_color="hazel"
+age_mine=14
+fav_subject="math"
+print(name, small_number, big_number, breackfast, fav_color ,school_name, year, eye_color, age_mine, fav_subject,)
