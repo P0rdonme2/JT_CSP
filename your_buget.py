3252d157cfda91100ad2422abf1b3130c348088a
P0rdonme2 <jaxon.thompson@ucas-edu.net>
2026-09-18 08:44:37 -0600

    as

--- your_buget.py
+++ your_buget.py
@@ -10,12 +10,13 @@ transportation = float(input("What is your monthly transportation:"))
 
 saving = income * 0.10
 
-print int((f"Your rent is {rent:.2f} and that is {rent/ income * 100}% of your income."))
+print((f"Your rent is {rent:.2f} and that is {rent/ income * 100}% of your income."))
 
 print(f"Your utilities is {utilities:.2f} and that is {utilities/income * 100}% of your income.")
 
 print(f"Your groceries is {groceries:.2f} and that is {groceries/income * 100}% of your income.")
 
 print(f"Your transportation is {transportation:.2f} and that is {transportation/income * 100}% of your income.")
-
-print("You should save {saving} a month, that is  of your income.")
+spending = (income - rent) - (income - utilities)
+print(f"You should save {saving} a month, that is  of your income.")
+print(f"You have $ 550.00 of spending money each month!") 
