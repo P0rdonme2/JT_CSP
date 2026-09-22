d00f059b168d7df76bacfc59d382650cb11a1f7a
P0rdonme2 <jaxon.thompson@ucas-edu.net>
2026-09-22 14:53:56 -0600

    asl

---
+++ conditionals.py
@@ -0,0 +1,27 @@
+# JT Conditionals Notes
+# Conditionals=helps code make desicions on a boolean statement
+# boolean = True or False
+
+time = 1416
+
+if time < 1200 and time > 500:
+    print('Good Morning!")
+    if day != 'Saterday' and day != "Sunday":
+        print("How has school been?")
+   
+elif time < 2000:
+    print('Good Evening')
+else:
+    print('Good Night!')
+ 
+print('Code is done')
+# Conditionals always begin with if then a boolean statement end it with a colan
+# If it holds a colan then indent in
+# Indented lines only happen if the condition above is True
+# Conditionals frequently end in a else
+# Out of conditional when indent ends
+# Else if the conditions above are false this will happen
+# Comparison operater = < less than, > greater than, == equal to, <= less than or equal to, >= greater than or equal to, != not equal
+# Logical Operators = and both conditions true, or atleast one must be true, not cheks if false
+# Elif only happens when the conditions above it is false
+# Nesting = one of something and put it in the same something
