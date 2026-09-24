4a0a44ed2d24c64c73b48a1c3b966a54782afeb8
P0rdonme2 <jaxon.thompson@ucas-edu.net>
2026-09-24 14:15:27 -0600

    sd

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,19 +1,19 @@
 # JT Password Strength Checker
 
 
-characters = False
+characters =False
 uppercase = False
 lowercase = False
 number = False
 symbol = False
 password = input("what is your password:")
-if len(password) >= 8
+if len(password) >= 8:
     characters = True
 for letter in password:
     if letter.isupper():
         uppercase = True
     if letter.islower():
-        lowercas = True
+        lowercase = True
     if letter.isnumeric():
         number = True
     if letter in "!?@#$%()[]<>":
@@ -23,8 +23,21 @@ print(f"At least 8 characters:{characters}")
 
 print(f"Has an uppercase letter: {uppercase}")
 
-Has a lowercase letter:
+print(f"Has a lowercase letter:{lowercase}")
 
-Has a number:
+print(f"Has a number:{number}")
 
-Has a symbol:
+print(f"Has a symbol:{symbol}")
+score = 0
+if len(password) >= 8:
+    score = 1
+    if letter.isupper():
+        score = 1
+    if letter.islower():
+        score = 1
+    if letter.isnumeric():
+        score = 1
+    if letter in "!?@#$%()[]<>":
+        score = 1
+
+print(f"Your password strength is:{score}")
